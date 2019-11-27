cbe78eae34e6e44730beecdc61dbff80ca443f53
Nikita <luzhbinnikita@gmail.com>
2019-11-28 01:58:12 +0300

    wip

--- Memery/Controller/Tabbar/TabbarController.swift
+++ Memery/Controller/Tabbar/TabbarController.swift
@@ -1,52 +1,77 @@
 import UIKit
 
-class TabbarController: UITabBarController {
+class TabbarController: UITabBarController, UITabBarControllerDelegate {
     
-    fileprivate enum TabBarItemType: Int {
-        case local = 0
-        case cloud = 1
-        case all = 2
-        case more = 3
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setupTabbar()
+
+        self.delegate = self
         initControllers()
     }
     
-    func setupTabbar() {
-        
-        tabBar.tintColor = .black
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+       
+        if let _ = viewController as? SearchViewController {
+            let searchVC = SearchViewController()
+            print("Controller presented")
+            self.present(searchVC, animated: true, completion: nil)
+            searchVC.modalPresentationStyle = .overFullScreen
+            return false
+        }
+//        if viewController.isKind(of: SearchViewController.self) {
+//            let searchVC = SearchViewController()
+//            self.present(searchVC, animated: true, completion: nil)
+//            return false
+//        }
+        return true
     }
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+
+        print(item.title)
+//        if let items = self.tabBar.items, let selectedIndex = items.firstIndex(of: item), priviousSelectedIndex != selectedIndex, let customTabBar = tabBar as? JBTabBar {
+//
+//        }
+    }
+    
+    
+    
+    //MARK: - Init Controllers
     
     func initControllers() {
         
         //TODO: Set selected image
         
         let localScreenStoryboard = UIStoryboard(name: "LocalScreen", bundle: nil)
-        let localVC = localScreenStoryboard.instantiateInitialViewController()
+        let localVC = localScreenStoryboard.instantiateInitialViewController() as! LocalViewController
         let localTabbarItem = UITabBarItem(title: "Local", image: #imageLiteral(resourceName: "localIcon"), selectedImage: nil)
-        localVC?.tabBarItem = localTabbarItem
+        localVC.tabBarItem = localTabbarItem
         
         let cloudScreenStoryboard = UIStoryboard(name: "CloudScreen", bundle: nil)
-        let cloudVC = cloudScreenStoryboard.instantiateInitialViewController()
+        let cloudVC = cloudScreenStoryboard.instantiateInitialViewController() as! CloudViewController
         let cloudTabbarItem = UITabBarItem(title: "Cloud", image: #imageLiteral(resourceName: "cloudIcon"), selectedImage: nil)
-        cloudVC?.tabBarItem = cloudTabbarItem
+        cloudVC.tabBarItem = cloudTabbarItem
+        
+        let searchScreenStoryboard = UIStoryboard(name: "SearchScreen", bundle: nil)
+        let searchVC = searchScreenStoryboard.instantiateInitialViewController() as! SearchViewController
+        let searchTabbarItem = UITabBarItem(title: "", image: #imageLiteral(resourceName: "baseline_search_black_48pt"), selectedImage: nil)
+        searchVC.tabBarItem = searchTabbarItem
         
         let allScreenStoryboard = UIStoryboard(name: "AllScreen", bundle: nil)
-        let allVC = allScreenStoryboard.instantiateInitialViewController()
+        let allVC = allScreenStoryboard.instantiateInitialViewController() as! AllViewController
         let allTabbarItem = UITabBarItem(title: "All", image: #imageLiteral(resourceName: "gridIcon"), selectedImage: nil)
-        allVC?.tabBarItem = allTabbarItem
+        allVC.tabBarItem = allTabbarItem
         
         let moreScreenStoryboard = UIStoryboard(name: "MoreScreen", bundle: nil)
-        let moreVC = moreScreenStoryboard.instantiateInitialViewController()
+        let moreVC = moreScreenStoryboard.instantiateInitialViewController() as! MoreViewController
         let moreTabbarItem = UITabBarItem(title: "More", image: #imageLiteral(resourceName: "moreIcon"), selectedImage: nil)
-        moreVC?.tabBarItem = moreTabbarItem
+        moreVC.tabBarItem = moreTabbarItem
         
-        let viewControllers = [localVC, cloudVC, allVC, moreVC]
+        let viewControllers = [localVC, cloudVC, searchVC, allVC, moreVC]
         
-        self.viewControllers = viewControllers.map { UINavigationController(rootViewController: $0!)}
+        self.viewControllers = viewControllers
+        
+        //self.viewControllers = viewControllers.map { UINavigationController(rootViewController: $0)}
     }
 }
