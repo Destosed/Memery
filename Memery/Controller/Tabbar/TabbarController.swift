0631a411e4c6e413e423e6814504f057fa670fad
Nikita <luzhbinnikita@gmail.com>
2019-11-28 17:11:03 +0300

    [Add] image in seleted tabbar items

--- Memery/Controller/Tabbar/TabbarController.swift
+++ Memery/Controller/Tabbar/TabbarController.swift
@@ -12,10 +12,17 @@ class TabbarController: UITabBarController, UITabBarControllerDelegate {
         setupMiddleButton()
     }
     
+    //MARK: - TabbarController Delegate
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        
+    }
+    
     //MARK: - Setups
     
     func setupTabbar() {
         tabBar.tintColor = .white
+        tabBar.unselectedItemTintColor = .white //.init(white: 0.9999, alpha: 0.7)
     }
     
     func setupMiddleButton() {
@@ -44,12 +51,16 @@ class TabbarController: UITabBarController, UITabBarControllerDelegate {
         
         let localScreenStoryboard = UIStoryboard(name: "LocalScreen", bundle: nil)
         let localVC = localScreenStoryboard.instantiateInitialViewController() as! LocalViewController
-        let localTabbarItem = UITabBarItem(title: "", image: #imageLiteral(resourceName: "baseline_laptop_mac_white_48pt"), selectedImage: nil)
+        let localIcon = UIImage(named: "localIcon")
+        let localIcon2x = UIImage(named: "localIcon2x")
+        let localTabbarItem = UITabBarItem(title: "", image: localIcon, selectedImage: localIcon2x)
         localVC.tabBarItem = localTabbarItem
         
         let cloudScreenStoryboard = UIStoryboard(name: "CloudScreen", bundle: nil)
         let cloudVC = cloudScreenStoryboard.instantiateInitialViewController() as! CloudViewController
-        let cloudTabbarItem = UITabBarItem(title: "", image: #imageLiteral(resourceName: "outline_cloud_white_48pt"), selectedImage: nil)
+        let cloudIcon = UIImage(named: "cloudIcon")
+        let cloudIcon2x = UIImage(named: "cloudIcon2x")
+        let cloudTabbarItem = UITabBarItem(title: "", image: cloudIcon, selectedImage: cloudIcon2x)
         cloudVC.tabBarItem = cloudTabbarItem
         
         let searchVC = SearchViewController()
@@ -59,12 +70,16 @@ class TabbarController: UITabBarController, UITabBarControllerDelegate {
         
         let allScreenStoryboard = UIStoryboard(name: "AllScreen", bundle: nil)
         let allVC = allScreenStoryboard.instantiateInitialViewController() as! AllViewController
-        let allTabbarItem = UITabBarItem(title: "", image: #imageLiteral(resourceName: "outline_grid_on_white_48pt"), selectedImage: nil)
+        let allIcon = UIImage(named: "gridIcon")
+        let allIcon2x = UIImage(named: "gridIcon2x")
+        let allTabbarItem = UITabBarItem(title: "", image: allIcon, selectedImage: allIcon2x)
         allVC.tabBarItem = allTabbarItem
         
         let moreScreenStoryboard = UIStoryboard(name: "MoreScreen", bundle: nil)
         let moreVC = moreScreenStoryboard.instantiateInitialViewController() as! MoreViewController
-        let moreTabbarItem = UITabBarItem(title: "", image: #imageLiteral(resourceName: "outline_more_horiz_white_48pt"), selectedImage: nil)
+        let moreIcon = UIImage(named: "moreIcon")
+        let moreIcon2x = UIImage(named: "moreIcon2x")
+        let moreTabbarItem = UITabBarItem(title: "", image: moreIcon, selectedImage: moreIcon2x)
         moreVC.tabBarItem = moreTabbarItem
         
         let viewControllers = [localVC, cloudVC, searchVC, allVC, moreVC]
